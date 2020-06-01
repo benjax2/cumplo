7f9502d699bf7752f46f4228d4bd6458b3962e87
Benjamin Antonio Navarrete Mena <benja@Benjamins-MacBook-Pro.local>
2020-06-01 15:22:50 -0400

    corregido error en swal

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,54 +6,63 @@ import swal from 'sweetalert';
 
 
 class Home extends Component {
+    componentDidMount() {
+        return (
+            swal(
+
+                '¡ Bienvenido !',
+                'En esta aplicacion podras saber el valor del dolar entre dos fechas determinadas por ti y ademas saber el promedio entre ellas!'
+            )
+        );
+    }
     constructor(props) {
         super(props);
-        this.state = {fecha1: null,fecha2:null,data:[],labels:[]};
-      }
-      onFetch = ({data,labels}) =>{
+        this.state = { fecha1: null, fecha2: null, data: [], labels: [] };
+    }
+    onFetch = ({ data, labels }) => {
         const sum = data.reduce((a, b) => a + b, 0);
         const avg = (sum / data.length) || 0;
         swal(
-            'El promedio es: '+Math.round(avg),
+            'El promedio es: ' + Math.round(avg),
             '',
             'success'
         )
-          this.setState({data,labels})
-      }
-      handleChange1 = (event) =>  {
-        const date = new Date(event.target.value);
-        this.setState({fecha1: date});
-        console.log({date});
+        this.setState({ data, labels })
     }
-      handleChange2 = (event) =>  {
+    handleChange1 = (event) => {
         const date = new Date(event.target.value);
-        this.setState({fecha2: date});
+        this.setState({ fecha1: date });
+        console.log({ date });
+    }
+    handleChange2 = (event) => {
+        const date = new Date(event.target.value);
+        this.setState({ fecha2: date });
         console.log(date);
-      }
+    }
     render() {
 
         return (
 
             <div className="home">
                 <div className="container mt-5">
-                <h1>Precio del dolar </h1>
+                    <h1>Precio del dolar </h1>
                     <br />
                     <form onSubmit={this.handleSubmit}>
                         <div className="form-group">
                             <label>
-                                Fecha1:  
-                                <br/><input type="date" id="fecha1" onChange={this.handleChange1} />
+                                Fecha1:
+                                <br /><input type="date" id="fecha1" onChange={this.handleChange1} />
                             </label>
                         </div>
                         <div className="form-group">
                             <label>
                                 Fecha2:
-                                <br/> <input type="date" id="fecha2" onChange={this.handleChange2} />
+                                <br /> <input type="date" id="fecha2" onChange={this.handleChange2} />
                             </label>
                         </div>
                         <div className="clearfix"></div>
                         <Grafico fecha1={this.state.fecha1}
-                        fecha2={this.state.fecha2} onFetch={this.onFetch} />
+                            fecha2={this.state.fecha2} onFetch={this.onFetch} />
                     </form>
                     {this.state.data.length > 0 &&
                         <Chart data={this.state.data} labels={this.state.labels} />
